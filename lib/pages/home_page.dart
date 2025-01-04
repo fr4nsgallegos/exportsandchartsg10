f607733f0c954c16b4e8ff966b082084cffead12
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-01-03 22:03:54 -0500

    export simple pdf

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,64 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:open_filex/open_filex.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  Future<File> generatePdf() async {
+    //Crear dicumento PDF
+    final pdf = pw.Document();
+
+    //Añadiendo contenido al documento
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Text("Hola, este en un PDF creado desde FLUTTER!!!!"),
+          );
+        },
+      ),
+    );
+
+    //Obtener la ruta de almacenamiento local
+    final output = await getApplicationDocumentsDirectory();
+    final file = File("${output.path}/example.pdf");
+
+    //guardar el archivvo
+    await file.writeAsBytes(await pdf.save());
+    print("pdf guardado en ${file.path}");
+    return file;
+  }
+
+  void openPdfFile(File pdfFile) async {
+    try {
+      print("Intentando abrir el pdf");
+      final result = await OpenFilex.open(pdfFile.path);
+      print("resultado al abrir: $result");
+    } catch (e) {
+      print("error: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                final pdfFile = await generatePdf();
+                openPdfFile(pdfFile);
+              },
+              child: Text("Esportar a PDF"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
