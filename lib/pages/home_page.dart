301d67b16da87f4b4c9284e2379dff7a35060dcd
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-01-03 22:08:40 -0500

    pdf with tablw

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -32,6 +32,35 @@ class HomePage extends StatelessWidget {
     return file;
   }
 
+  Future<File> generateTablePdf() async {
+    final pdf = pw.Document();
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) {
+          return pw.Table.fromTextArray(
+            headers: ["Id", "Nombre", "Puntuación"],
+            data: [
+              [1, "Juan", 95],
+              [2, "Pedro", 80],
+              [3, "Elias", 54],
+              [4, "María", 70],
+              [5, "Olenka", 100],
+            ],
+          );
+        },
+      ),
+    );
+
+    //Obtener la ruta de almacenamiento local
+    final output = await getApplicationDocumentsDirectory();
+    final file = File("${output.path}/example.pdf");
+
+    //guardar el archivvo
+    await file.writeAsBytes(await pdf.save());
+    print("pdf guardado en ${file.path}");
+    return file;
+  }
+
   void openPdfFile(File pdfFile) async {
     try {
       print("Intentando abrir el pdf");
@@ -55,6 +84,13 @@ class HomePage extends StatelessWidget {
                 openPdfFile(pdfFile);
               },
               child: Text("Esportar a PDF"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final pdfFile = await generateTablePdf();
+                openPdfFile(pdfFile);
+              },
+              child: Text("Esportar a PDF con tabla"),
             ),
           ],
         ),
